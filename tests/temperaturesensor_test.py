7d6e4aae8d68e0c568be7a38c21a0f6adec94d0f
Chad Rosenquist <chadrosenquist@gmail.com>
2018-05-12 15:51:12 -0500

    Test TemperatureSensor by patching calls to Adafruit_DHT module.

--- tests/temperaturesensor_test.py
+++ tests/temperaturesensor_test.py
@@ -1,6 +1,7 @@
 """Tests the TemperatureSensor class."""
 
 import unittest
+from unittest.mock import patch, MagicMock
 from configparser import ConfigParser
 
 from loggingtestcase import capturelogs
@@ -69,6 +70,60 @@ class TemperatureSensorFromConfigTestCase(unittest.TestCase):
                                     r"valid model!  Valid models are: "
                                     r"\('DHT11', 'DHT22', 'AM2302'\)"):
             TemperatureSensor.from_config(cfg)
+
+
+class TemperatureSensorWithPatchTestcase(unittest.TestCase):
+    """Tests patching calls to Adafruit_DHT module."""
+    @capturelogs('homemonitor.sensor', 'DEBUG')
+    def test_alarm_off(self, logs):
+        """Alarm is off."""
+        # Module Adafruit_DHT is only installed on Raspberry Pi, so mock it!
+        adafruit_patch = MagicMock()
+        adafruit_patch.read_retry = MagicMock(return_value=(0, 16))
+        adafruit_patch.DHT22 = 'MockDTH22'
+        with patch.dict("sys.modules", Adafruit_DHT=adafruit_patch):
+            sensor = TemperatureSensor('TEST', 55, 4, 'DHT22')
+            sensor.status()
+
+            # Verify alarm and hw error are correct.
+            self.assertFalse(sensor.alarm_on,
+                             'The alarm should not be on.')
+            self.assertFalse(sensor.hw_error_on,
+                             'The hardware error alarm should not be on.')
+
+            # Verify read_retry() was called with correct arguments.
+            self.assertEqual('MockDTH22',
+                             adafruit_patch.read_retry.call_args_list[0][0][0],
+                             'adafruit_patch.read_retry() should be called with sensor type '
+                             'MockDTH22.')
+            self.assertEqual(4,
+                             adafruit_patch.read_retry.call_args_list[0][0][1],
+                             'adafruit_patch.read_retry() should be called with gpio pin 4.')
+
+            # Verify logging is correct.
+            self.assertEqual('DEBUG:homemonitor.sensor:TemperatureSensor TEST is detecting 60 '
+                             'degrees Fahrenheit',
+                             logs.output[1])
+
+    def test_alarm_on(self):
+        """Alarm is on."""
+        adafruit_patch = MagicMock()
+        adafruit_patch.read_retry = MagicMock(return_value=(0, 10))
+        with patch.dict("sys.modules", Adafruit_DHT=adafruit_patch):
+            sensor = TemperatureSensor('TEST', 55, 4, 'DHT22')
+            sensor.status()
+            self.assertTrue(sensor.alarm_on,
+                            'The alarm should be on.')
+
+    def test_hw_error_on(self):
+        """Hardware error is on."""
+        adafruit_patch = MagicMock()
+        adafruit_patch.read_retry = MagicMock(return_value=(None, None))
+        with patch.dict("sys.modules", Adafruit_DHT=adafruit_patch):
+            sensor = TemperatureSensor('TEST', 55, 4, 'DHT22')
+            sensor.status()
+            self.assertTrue(sensor.hw_error_on,
+                            'The hardware error should be on.')
 
 
 class TemperatureSensorManualTestcase(unittest.TestCase):
